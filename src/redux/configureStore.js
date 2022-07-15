c2c14d09b27b71c4c96d3b3dde669d9c5036b759
Saied Gaffer <saied2421998@gmail.com>
2022-07-15 17:57:07 +0200

    setup redux and connect it with react

---
+++ src/redux/configureStore.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({// put any reducers here spreated by comma});
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
