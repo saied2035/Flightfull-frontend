8d75096e65560b54605f4791f73121fd511b1835
Saied Gaffer <saied2421998@gmail.com>
2022-07-15 18:47:06 +0200

    add react router and connect it to the react app & delete unnescessary files & create header component for navigation panel

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+  <NavLink to="/">
+    Home
+  </NavLink>
+);
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
 );
