c1983efceaa038eaa063e7b59064443c6396f595
Zahra Arshia <fzarshia@gmail.com>
2022-07-21 16:09:37 +0430

    Update configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from './users/users';
-import flightsReducers from './flights/flights';
+import flightsReducer from './flights/flights';
 
-const rootReducer = combineReducers({ usersReducer, flightsReducers });
+const rootReducer = combineReducers({ usersReducer, flightsReducer });
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
