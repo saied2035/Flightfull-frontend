4b59899e2a1206172c102db3b6558b6299b6644c
Thermiee <navyblack16@yahoo.com>
2022-07-23 23:07:43 +0100

    create add item nav link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import { NavLink } from 'react-router-dom';
 
 const Header = () => (
+  <>
   <NavLink to="/">
     Home
-  </NavLink>
+  </NavLink><NavLink to="/addItem">
+      Add Item
+    </NavLink>
+    </>
 );
 
 export default Header;
