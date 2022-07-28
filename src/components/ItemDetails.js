35e8dfabdf2c554614aea15b2b48c0cfc742cd11
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-07-28 14:30:23 +0430

    add details page styles and back button

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,4 +1,4 @@
-import { useParams, NavLink, useLocation } from 'react-router-dom';
+import { useParams, Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const DetailsPage = () => {
@@ -8,23 +8,49 @@ const DetailsPage = () => {
   const { item } = state || { item: createdFlight } || { item: null };
   return (
     item && (
-    <div className="reservation">
-      <p>
-        Flight name:
-        {item.name}
-      </p>
-      <p>
-        Flight number:
-        {item.flight_number}
-      </p>
-      <img alt="flight-pic" src={item.image} />
-      <p>
-        Price:
-        {item.price}
-      </p>
-      <NavLink to="/reservations/add" state={{ item_id: `${id}` }}>
-        Reserve
-      </NavLink>
+    <div className="detailMain">
+      <div>
+        <img className="detailPic" alt="flight-pic" src={item.image} />
+        <Link to="/">
+          <button
+            type="button"
+            className="CustomPrevArrow"
+          >
+            <img alt="prev" src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/36/ffffff/external-Chevron-arrows-tanah-basah-glyph-tanah-basah-4.png" />
+          </button>
+        </Link>
+      </div>
+      <div className="cardDetails">
+        <ul className="detailsList">
+          <li>
+            Flight name:
+            {' '}
+            {item.name}
+          </li>
+          <li>
+            Flight number:
+            {' '}
+            {item.flight_number}
+          </li>
+          <li>
+            Price:
+            {' '}
+            $
+            {item.price}
+          </li>
+        </ul>
+        <Link to="/reservations/add" state={{ item_id: `${id}` }}>
+          <button
+            type="button"
+            className="reserve-btn"
+          >
+            Reserve
+          </button>
+        </Link>
+        {/* <NavLink to="/reservations/add" state={{ item_id: `${id}` }}>
+          Reserve
+        </NavLink> */}
+      </div>
     </div>
     )
   );
